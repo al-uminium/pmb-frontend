b1e2ebc805a4ac68bb3312d6b73e41871989bba0
al-uminium <aeronafic@gmail.com>
2024-07-08 12:24:42 +0800

    initialized settle payments components

---
+++ src/app/component/settle-payment/settle-payment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settle-payment',
+  standalone: true,
+  imports: [],
+  templateUrl: './settle-payment.component.html',
+  styleUrl: './settle-payment.component.css'
+})
+export class SettlePaymentComponent {
+
+}
