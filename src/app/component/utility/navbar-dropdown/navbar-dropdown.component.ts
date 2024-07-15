52991afa68a1d99940117fd604eadce15636f30a
al-uminium <aeronafic@gmail.com>
2024-07-15 19:39:57 +0800

    added navbar components

---
+++ src/app/component/utility/navbar-dropdown/navbar-dropdown.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroEllipsisVertical } from '@ng-icons/heroicons/outline';
+import { Store, StoreModule } from '@ngrx/store';
+import { logoutUser } from '../../../state/user.actions';
+
+@Component({
+  selector: 'app-navbar-dropdown',
+  standalone: true,
+  imports: [NgIconComponent, StoreModule],
+  providers: [provideIcons({ heroEllipsisVertical })],
+  templateUrl: './navbar-dropdown.component.html',
+  styleUrl: './navbar-dropdown.component.css'
+})
+export class NavbarDropdownComponent {
+  private readonly store = inject(Store)
+  showDropdown: boolean = false;
+
+  toggleDropdown(): void {
+    this.showDropdown = !this.showDropdown;
+  }
+
+  handleLogOut(): void {
+    this.store.dispatch(logoutUser());
+  }
+}
