87d0f66147832539187bf61161b5595f35015757
al-uminium <aeronafic@gmail.com>
2024-07-15 19:40:29 +0800

    did some restyling due to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingpageComponent } from './component/landingpage/landingpage.component';
+import { NavbarComponent } from './component/utility/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingpageComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
